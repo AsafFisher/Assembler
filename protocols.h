1ed3bd7e1c5c94fc54b10da5b72af8acf8aafd33
Asaf Fisher <asaffisher@icloud.com>
2016-06-27 12:22:15 -0700

    ubuntu test

--- protocols.h
+++ protocols.h
@@ -1,15 +1,29 @@
-
-typedef Command struct{
-	unsigned int ERA:2
-	unsigned int destar:2
-	unsigned int srcar:2
-	unsigned int opcode:4
-	unsigned int grp:2
-	unsigned int dumy:3
-}
 typedef Word struct{
 	unsigned int cell:15
 }
+
+typedef union Command{
+	struct{
+
+		unsigned int ERA:2
+		unsigned int destar:2
+		unsigned int srcar:2
+		unsigned int opcode:4
+		unsigned int grp:2
+		unsigned int dumy:3
+	} cmd
+	Word word;
+}
+typedef union Parameter{
+	struct{
+		
+	}
+	Word word;
+}
+
+
+enum{INSTANT_ADRESS_RESOLUTION = 0,DIRECT_ADRESS_RESOLUTION,INSTANT_ADRESS_RESOLUTION,REGISTER_DIRECT_ADRESS_RESOLUTION}
+enum{ABSULUT,}
 void setWord(Word word, int number);
 void setCommand(Word word)
 Command createInstaceOfCommand(int pERA,int pdestar,int psrcar,int popcode,int pgrp);
