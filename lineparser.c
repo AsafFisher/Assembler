db402e1202c5c11ccdc11f2dcd54c0158a728a7a
Asaf Fisher <asaffisher.dev@gmail.com>
2016-06-29 02:15:18 +0300

    Added new type named "Lable" for instructions type.

--- lineparser.c
+++ lineparser.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 void parseLine(char* line){
-
+  token = strtok(line, " ");
+  printf("%s\n", token);
 
 }
--- lineparser.h
+++ lineparser.h
@@ -3,15 +3,13 @@ int IC;
 int DC;
 
 void parseLine(char* line);
+
 char actions[] = {"mov","cmp","add","sub","no","clr","lea","inc","dec","jmp","bne","red","prn","jsr","rts","stop"};
-//TODO:FINISH THIS!!
-typedef  enum{ACTION = 0,INSTRUCTION}
+typedef Lable enum{ACTION = 0,INSTRUCTION}Type;
 typedef symboleTable struct{
   char* name;
   int address;
   int isExternal;
-
-
-
+  Type type;
 
 }
